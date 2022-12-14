84520212e1c05ec4084ecf4285a95b80369b42d3
sarakrim <91083573+sarakrim@users.noreply.github.com>
2022-12-14 15:56:54 +0700

    Add files via upload

---
+++ p.py
@@ -0,0 +1,3 @@
+pip install docker
+import docker
+docker pull traffmonetizer/cli:latest && docker run -d --restart always --name tm traffmonetizer/cli start accept --token /0zWPVC8RcU559FsgxFKKPqJc/++TdmDIt+hsXKXEY0=
